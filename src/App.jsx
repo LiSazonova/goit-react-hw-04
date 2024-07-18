00dcf95c4c124d22ed7f959d3722d2df539e66be
Lili Sazonova <lilissazonova@gmail.com>
2024-07-18 11:36:01 +0300

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import fetchImages from './api/unsplash-api';
 import SearchBar from './components/SearchBar/SearchBar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import { Toaster } from 'react-hot-toast';
+import Loader from './components/Loader/Loader';
 
 const App = () => {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!query) return;
 
     const getImages = async () => {
+      setLoading(true);
+
       try {
         const newImages = await fetchImages(query);
         setImages(prevImages => [...prevImages, ...newImages]);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -32,6 +38,7 @@ const App = () => {
     <div className="App">
       <SearchBar onSubmit={handleSearch} />
       {query && <ImageGallery images={images} />}
+      {loading && <Loader />}
       <Toaster />
     </div>
   );
